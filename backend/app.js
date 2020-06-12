f332c481732057cb3c2cb2b68763ee80f205c1b5
TSY <example@mail.com>
2020-06-12 10:44:21 +0800

    remove config file

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
 var nodemailer = require('nodemailer');
-const creds = require('../config/config');
+// const creds = require('../config/config');
 
 var transport = {
     host: 'smtp.gmail.com',
     auth: {
-        user: creds.USER,
-        pass: creds.PASS
+        user: process.env.USER,
+        pass: process.env.PASS
     }
 }
 
